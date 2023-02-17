5c0fe236835677e5ac7bbf9b8e34532f76142421
peterouob <hellolinpeter@gmail.com>
2023-02-17 15:49:34 +0800

    新增routerGroup

--- gdefer/gdefer.go
+++ gdefer/gdefer.go
@@ -6,27 +6,51 @@ import (
 
 type HandleFunc func(ctx *Context)
 
+type RouterGroup struct {
+	prefix     string
+	middleware []HandleFunc
+	parent     *RouterGroup
+	engine     *Engine
+}
+
 type Engine struct {
+	*RouterGroup
 	router *Router
+	groups []*RouterGroup
 }
 
 func New() *Engine {
-	return &Engine{router: newRouter()}
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
 }
 
-func (e *Engine) addRouter(method, path string, handle HandleFunc) {
-	e.router.addRouter(method, path, handle)
+func (g *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := g.engine
+	newGroup := &RouterGroup{
+		prefix: g.prefix + prefix,
+		parent: g,
+		engine: engine,
+	}
+	return newGroup
+}
+
+func (g *RouterGroup) addRouter(method, comb string, handle HandleFunc) {
+	pattern := g.prefix + comb
+	//log.Printf("method [%s] pattern [%s]", method, pattern)
+	g.engine.router.addRouter(method, pattern, handle)
+}
+
+func (g *RouterGroup) Get(pattern string, handle HandleFunc) {
+	g.addRouter("GET", pattern, handle)
+}
+func (g *RouterGroup) Post(pattern string, handle HandleFunc) {
+	g.addRouter("POST", pattern, handle)
 }
 
 func (e *Engine) Run(addr string) error {
 	return http.ListenAndServe(addr, e)
-}
-
-func (e *Engine) Get(pattern string, handle HandleFunc) {
-	e.addRouter("GET", pattern, handle)
-}
-func (e *Engine) Post(pattern string, handle HandleFunc) {
-	e.addRouter("POST", pattern, handle)
 }
 
 func (e *Engine) ServeHTTP(w http.ResponseWriter, r *http.Request) {
